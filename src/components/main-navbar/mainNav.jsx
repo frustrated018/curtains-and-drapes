0daa5110b25e06d24191bee14eeebaa321f82b63
frustrated018 <niloyd018@gmail.com>
2024-01-21 23:50:17 +0600

    Added navigation menu to navbar

--- src/components/main-navbar/mainNav.jsx
+++ src/components/main-navbar/mainNav.jsx
@@ -1,33 +1,93 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+import Link from "next/link";
+
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const MainNav = () => {
   return (
-    <nav className="bg-slate-300 p-[.5rem]">
+    <nav className="p-[.5rem]">
       <section className="flex justify-between items-center mx-5">
         {/* //* Logo */}
-        <h4 className="text-2xl font-bold">Curtains & Drapes</h4>
+        <h4 className="text-2xl font-bold">C&D</h4>
 
         {/* Links will be added here */}
-        {/* //! useful for  */}
-        <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+
+        {/* <div className="hidden md:flex gap-2">
+          <Link href="#">About</Link>
+          <Link href="#">Contact</Link>
+          <Link href="#">Deals</Link>
+        </div> */}
+
+        <div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/#">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/#">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Contact
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/#">
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Deals
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+        {/* //! For Mobile devices  */}
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <GiHamburgerMenu size={24} />
+            </SheetTrigger>
+            <SheetContent side={"left"}>
+              <SheetHeader>
+                <SheetTitle>Curtains & Drapes</SheetTitle>
+              </SheetHeader>
+              {/* //! links */}
+            </SheetContent>
+          </Sheet>
+        </div>
 
         {/* //! Avatar from shadcn */}
         <div className="">
