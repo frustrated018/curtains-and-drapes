f41b0afba09a4f8dc4ab2386a26d865175c09e61
frustrated018 <niloyd018@gmail.com>
2024-01-21 20:43:40 +0600

    successfully converted the avater to a dropdown

--- src/components/main-navbar/mainNav.jsx
+++ src/components/main-navbar/mainNav.jsx
@@ -1,20 +1,50 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const MainNav = () => {
   return (
     <nav className="bg-slate-300 p-[.5rem]">
-      <section className="flex justify-between items-center">
+      <section className="flex justify-between items-center mx-5">
         {/* //* Logo */}
-        <h4 className="text-2xl font-bold">My Logo</h4>
+        <h4 className="text-2xl font-bold">Curtains & Drapes</h4>
 
         {/* Links will be added here */}
+        {/* //! useful for  */}
+        <DropdownMenu>
+          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
         {/* //! Avatar from shadcn */}
         <div className="">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>C&S</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>C&S</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+
+            {/* //! Content */}
+            <DropdownMenuContent>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Login</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </section>
     </nav>
