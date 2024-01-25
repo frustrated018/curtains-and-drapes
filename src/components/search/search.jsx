70fc76daf296f276d5fd9c13d2e620a55a85bb09
frustrated018 <niloyd018@gmail.com>
2024-01-26 00:27:49 +0600

    Added a magnefying icon

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -2,7 +2,7 @@ import { HiMagnifyingGlass } from "react-icons/hi2";
 
 const Search = () => {
   return (
-    <form className="w-max-[550px] w-full lg:w-80 xl:w-full">
+    <form className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
       <input
         type="text"
         name="search"
@@ -10,7 +10,9 @@ const Search = () => {
         autoComplete="off"
         className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
       />
-      
+      <div className="absolute right-0 top-0 mr-3 flex h-full items-center ">
+        <HiMagnifyingGlass />
+      </div>
     </form>
   );
 };
