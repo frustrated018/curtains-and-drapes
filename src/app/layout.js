b3b4a60b21dfba2d0d7613af62ce9df3636086f6
frustrated018 <niloyd018@gmail.com>
2024-01-25 20:48:58 +0600

    changed the theme to green

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/theme-toggle-button/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
