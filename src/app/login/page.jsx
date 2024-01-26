dbac4b2ccd215480133c12fac4879625b7660c27
frustrated018 <niloyd018@gmail.com>
2024-01-26 22:03:54 +0600

    done with the img section for now

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import LoginAuthForm from "./login-auth-form";
 import lightBg from "@/assets/login-light.jpg";
 import darkBg from "@/assets/login-dark.jpg";
+import Link from "next/link";
 
 const Login = () => {
   return (
     <section className="w-full min-h-screen max-w-screen-2xl mx-auto flex">
-        {/* //! Image related section */}
-      <section className="w-1/2 hidden lg:block">
+      {/* //! Image related section */}
+      <section className="w-1/2 hidden lg:block relative">
         <div className="max-h-screen">
           {/* //! Light image */}
           <Image
@@ -15,7 +16,7 @@ const Login = () => {
             alt="Authentication"
             fill
             objectFit="cover"
-            className="block dark:hidden opacity-75"
+            className="block dark:hidden opacity-85"
           />
           {/* //! Dark image */}
           <Image
@@ -23,10 +24,20 @@ const Login = () => {
             alt="Authentication"
             fill
             objectFit="cover"
-            className="hidden dark:block opacity-75"
+            className="hidden dark:block opacity-50"
           />
           {/* //! Text  */}
 
+          <Link href="/" className="text-3xl font-bold absolute top-10 left-10">
+            Curtains & Drapes
+          </Link>
+
+          <div className="absolute bottom-10 left-10">
+            <p className="w-[80%] text-lg font-semibold">
+              Unlock a world of comfort and style. Join us to discover the
+              finest home essentials for a truly inviting space.
+            </p>
+          </div>
         </div>
       </section>
       <div className="bg-background w-full lg:w-1/2 relative">
