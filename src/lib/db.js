dd900299180a3405fb5cd2c9bdc3ae3a734d2e65
frustrated018 <niloyd018@gmail.com>
2024-02-02 23:52:50 +0600

    Vercel deployment Error
    cant go to the /api/users route in deployed version
    trying to figure out why

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,28 +3,28 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connect = async () => {
-    const connectionState = mongoose.connection.readyState;
-  
-    if (connectionState === 1) {
-      console.log("Already connected");
-      return;
-    }
-  
-    if (connectionState === 2) {
-      console.log("Connecting...");
-      return;
-    }
-  
-    try {
-      mongoose.connect(MONGODB_URI, {
-        dbName: "Curtains&Drapes_DB",
-        bufferCommands: false,
-      });
-      console.log("Connected to DB");
-    } catch (error) {
-      console.log("Error in connecting to database", error);
-      throw new Error("Error connecting to database");
-    }
-  };
-  
-  export default connect;
+  const connectionState = mongoose.connection.readyState;
+
+  if (connectionState === 1) {
+    console.log("Already connected");
+    return;
+  }
+
+  if (connectionState === 2) {
+    console.log("Connecting...");
+    return;
+  }
+
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      dbName: "Curtains&Drapes_DB",
+      bufferCommands: false,
+    });
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log("Error in connecting to database", error);
+    throw new Error("Error connecting to database");
+  }
+};
+
+export default connect;
