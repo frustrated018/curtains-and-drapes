fd18d13428eb4b263a1e53fd7cf457f4bb545705
frustrated018 <niloyd018@gmail.com>
2024-02-02 22:10:20 +0600

    POST api success
    finally figured out the post api
    will be testing form the post request form the signup page shortly

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,15 +1,40 @@
 import connect from "@/lib/db";
-import User from "@/lib/modals/user";
-import { NextResponse } from "next/server";
-
-
+import User from "@/lib/models/users";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async () => {
-    try {
-      await connect();
-      const users = await User.find();
-      return new NextResponse(JSON.stringify(users), { status: 200 });
-    } catch (error) {
-      return new NextResponse("Error in fetching users" + error, { status: 500 });
-    }
-  };
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Error in fetching users" + error, { status: 500 });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    const body = await request.json();
+    console.log(request);
+    console.log(body);
+
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "User is created", user: newUser }),
+      { status: 201 }
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({
+        message: "Error in creating user",
+        error,
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
