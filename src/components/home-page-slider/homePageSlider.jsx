1f528688fc073928d0432c84ebff9f0e61dce8f2
frustrated018 <niloyd018@gmail.com>
2024-02-03 20:37:07 +0600

    Slider content array
    Made an array to use as slider content
    will be editing the carousel component to make it more responsive

--- src/components/home-page-slider/homePageSlider.jsx
+++ src/components/home-page-slider/homePageSlider.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Carousel,
   CarouselContent,
@@ -7,24 +6,46 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
+import SliderCard from "./sliderCard";
+
+const sliderContents = [
+  {
+    url: "https://i.ibb.co/3Rx9WK8/dimas-anggara-VIk1nwibg-NE-unsplash.jpg",
+    name: "Dimas Anggara",
+    price: 25.99,
+  },
+  {
+    url: "https://i.ibb.co/PzQTMX3/jon-tyson-Slpsgi-Zs-SNk-unsplash.jpg",
+    name: "Jon Tyson",
+    price: 29.99,
+  },
+  {
+    url: "https://i.ibb.co/yh5Fm5C/mitchell-luo-b7hgn-Fu-X4-Rc-unsplash.jpg",
+    name: "Mitchell Luo",
+    price: 22.5,
+  },
+  {
+    url: "https://i.ibb.co/26YQMMS/anais-murith-u2k-V8mqvdtc-unsplash.jpg",
+    name: "Anais Murith",
+    price: 19.95,
+  },
+];
 
 const HomePageSlider = () => {
   return (
     <>
       <Carousel className="w-full max-w-screen-md xl:max-w-screen-lg mx-auto">
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
+          {sliderContents.map((content, index) => (
             <CarouselItem key={index}>
               <div className="p-5">
-                <Card>
-                  <CardContent className="flex aspect-video items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
+                <SliderCard content={content} />
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
+        <CarouselPrevious variant="ghost" />
+        <CarouselNext variant="ghost" />
       </Carousel>
     </>
   );
--- src/components/home-page-slider/sliderCard.jsx
+++ src/components/home-page-slider/sliderCard.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { Card, CardContent } from "@/components/ui/card";
 
-const SliderCard = () => {
-    return (
-        <div>
-            This will be the slider card 
-        </div>
-    );
+const SliderCard = ({ content }) => {
+//   console.log(content);
+
+  return (
+    <Card>
+      <CardContent className="flex flex-col aspect-video gap-5 bg-sky-200"></CardContent>
+    </Card>
+  );
 };
 
 export default SliderCard;
