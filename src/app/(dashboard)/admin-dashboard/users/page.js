9b6e176d76510ec3560d09166113e6f0cfc7e550
frustrated018 <niloyd018@gmail.com>
2024-02-09 22:54:59 +0600

    Can't build table without the typescrip

--- src/app/(dashboard)/admin-dashboard/users/page.js
+++ src/app/(dashboard)/admin-dashboard/users/page.js
@@ -1,10 +1,10 @@
-import UsersTable from "@/components/dashboard/users-table";
+import { Button } from "@/components/ui/button";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 const UsersPage = () => {
   return (
     <section className="p-3 lg:p-6">
-      {/* Breadcrumb Kinda sorta －_－ */}
+      {/* Breadcrumbs? Kinda sorta －_－ */}
       <div className="text-muted-foreground">
         <ol className="flex items-center">
           <li className="hover:cursor-pointer hover:text-primary">Dashboard</li>
@@ -13,7 +13,15 @@ const UsersPage = () => {
         </ol>
       </div>
 
-      <UsersTable />
+      <section className="mt-5 flex items-center justify-between border-b">
+        <div className="mb-2 space-y-1">
+          <h2 className="text-3xl font-semibold">Users(10)</h2>
+          <p className="text-muted-foreground">All the users in database.</p>
+        </div>
+        <Button>Add User</Button>
+      </section>
+
+      {/* //TODO: Need to build a table using tanstack table and shadcn ui but don't know how to do it without typescript. ahhhhhhh */}
     </section>
   );
 };
