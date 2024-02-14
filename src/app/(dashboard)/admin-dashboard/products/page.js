0fc76dadeaa032bb0fa2803102ee507eed8c31b6
frustrated018 <niloyd018@gmail.com>
2024-02-14 22:01:39 +0600

    Added the base ui
    Added the top part of the products page
    Will start work on products table soon

--- src/app/(dashboard)/admin-dashboard/products/page.js
+++ src/app/(dashboard)/admin-dashboard/products/page.js
@@ -1,10 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const ProductsPage = () => {
-    return (
-        <div>
-            <h2 className="text-4xl text-center mt-20">Products page</h2>            
+  return (
+    <section className="min-h-screen p-3 lg:p-6">
+      {/* Breadcrumbs? Kinda sorta －_－ */}
+      <div className="text-muted-foreground">
+        <ol className="flex items-center">
+          <li className="hover:cursor-pointer hover:text-primary">Dashboard</li>
+          <MdKeyboardArrowRight size={20} />
+          <li className="hover:cursor-pointer hover:text-primary">Products</li>
+        </ol>
+      </div>
+
+      <section className="mt-5 flex items-center justify-between border-b">
+        <div className="mb-2 space-y-1">
+          <h2 className="text-3xl font-semibold">Products (100)</h2>
+          <p className="text-muted-foreground">All the Products in database.</p>
         </div>
-    );
+        <Button>Add Product</Button>
+      </section>
+      {/* //* Table  */}
+    </section>
+  );
 };
 
 export default ProductsPage;
